21a4fdb6143fe0acd110b7cae7c0744420c43ed8
JackieCasper <Casper.Jaclyn@gmail.com>
2017-01-28 14:55:10 -0500

    Cards now generated with JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,14 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+var gameBoard = document.getElementById("game-board");
+createCards();
+
+function createCards() {
+	for (var x = 0; x < 4; x++) {
+		var card = document.createElement("div");
+		card.className = "card";
+		gameBoard.appendChild(card);
+	}
+}
